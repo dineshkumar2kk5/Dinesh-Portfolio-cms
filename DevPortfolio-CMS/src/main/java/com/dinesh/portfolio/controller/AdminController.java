b1f05f825f88f37ed89123b8d697a161792baa21
Dinesh Kumar <dineshkumar2kk5@gmail.com>
2026-08-10 20:18:42 +0530

    Fix ResumeService import in AdminController

--- DevPortfolio-CMS/src/main/java/com/dinesh/portfolio/controller/AdminController.java
+++ DevPortfolio-CMS/src/main/java/com/dinesh/portfolio/controller/AdminController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.Optional;
 
 import com.dinesh.portfolio.model.Resume;
+import com.dinesh.portfolio.service.ResumeService;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
